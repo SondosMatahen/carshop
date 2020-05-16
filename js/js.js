43aef891f1f48cff1d7f53fd43b786de8df91d54
SondosMatahen <sondosmatahen@gmail.com>
2020-05-16 16:31:06 +0300

    loop

--- js/js.js
+++ js/js.js
@@ -1,5 +1,7 @@
 alert('Thank you for your visiting')
-var Day = prompt('what is the day (Sunday-Wednesday)?+First letter capital')
+
+
+var Day = prompt('what is the day (Sunday-Wednesday)?\nFirst letter capital')
 var Today ;
 switch (Day)
 {
@@ -19,14 +21,30 @@ switch (Day)
         Today =("Wednesday")
         break
 
-    default:
-        Today =( 'Anotherday')
-        break 
+    /*default:
+        Today =( 'Holiday')
+        break */  
     
 }
 
+
+/*while (!( == 'Sunday' || Day =='Monday' || Day =="Tuesday")) {
+     Day = prompt (' You should choose from Sunday - Tuesday')
+}
+*/
+while (!(Today == 'Sunday' || Today =='Monday' || Today =="Tuesday")) {
+     Today = prompt (' You should choose from Sunday - Tuesday')
+}
 document.write('<h4>'+' Today is '+"  "+ Today+'</h4>')
-
-
 console.log(Today)
+
+var car ;
+function carNum (){
+    var Num = prompt ('How many car do you need ?')
+    for (var i=0 ; i< Num ; i++){
+  car ='<img src="https://www.market2cars.com/wp-content/uploads/2019/11/960x0.jpg">'
+  document.write(car)}
+    
+}
+carNum()
 confirm("Press a button!\nEither OK or Cancel.");
